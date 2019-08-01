f2ecc61924e7d5c17963ed9ed4c5446d618f1ad9
vjola <vjola@tavisca.com>
2019-08-01 12:22:07 +0530

    Add project files.

---
+++ Webserver_using_tcp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Webserver_using_tcp
+{
+    class Server
+    {
+            private TcpListener Listener;
+            private int port = 8080;
+            public Server()
+            {
+                    try
+                    {
+                             Listener = new TcpListener(port);
+                             Listener.Start();
+                             Console.WriteLine("Running Server");
+                             Thread th = new Thread(StartListen);
+                             th.Start();
+
+                    }
+                    catch(Exception e)
+                    {
+                             Console.WriteLine(e.ToString());
+                    }
+            }
+
+        
+    }
+}
+    
